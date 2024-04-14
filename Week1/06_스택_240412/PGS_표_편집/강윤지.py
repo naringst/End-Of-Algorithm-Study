66a3a3c392cdc0d7e7fe490969106ed139e5a1a5
dbswl701 <dbswl701@ajou.ac.kr>
2024-04-14 23:37:35 +0900

    [PGS] 표 편집 / lv3 / -분 / 260.98ms / 강윤지

--- Week1/06_스택_240412/PGS_표_편집/강윤지.py
+++ Week1/06_스택_240412/PGS_표_편집/강윤지.py
@@ -9,3 +9,36 @@
 
 # 실행시간 
 ## ms~ ms
+
+def solution(n, k, cmd):
+    deleted = []
+    
+    for command in cmd:
+        line = command.split()
+        
+        if line[0] == "D":
+            k += int(line[1])
+            # 행 벗어나는지 판단
+            if k > n - len(deleted):
+                k = n - len(deleted)
+        if line[0] == 'U':
+            k -= int(line[1])
+            # 행 벗어나는지 판단
+            if k < 0:
+                k = 0
+        if line[0] == 'C':
+            deleted.append(k)
+            # 만약, 제일 마지막거 지운다면, k는 -1
+            if k == n - len(deleted):
+                k -= 1
+        if line[0] == 'Z':
+            item = deleted.pop()
+            # 만약 이게 현재 k 보다 작다면, k 는 추가
+            if item < k:
+                k += 1
+    
+    answer = ['O' for _ in range(n)]
+    for item in deleted:
+        answer[item] = 'X'
+        
+    return "".join(answer)

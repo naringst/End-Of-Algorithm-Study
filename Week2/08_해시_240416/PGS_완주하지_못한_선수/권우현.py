4e7308dac75d9bf79c3af98333457674010efa86
kwh <kwh871005@gmail.com>
2024-04-16 22:10:27 +0900

    [240416] 08장 해시 / 권우현

--- Week2/08_해시_240416/PGS_완주하지_못한_선수/권우현.py
+++ Week2/08_해시_240416/PGS_완주하지_못한_선수/권우현.py
@@ -1,11 +1,24 @@
 # 아이디어
-## 
+## 정렬해서 다르면 return
 
 # 예상 시간복잡도
-##
+## nlogn
 
-# 풀이시간 
-## 분
+# 풀이시간
+## 10분
 
-# 실행시간 
-## ms~ ms
+# 실행시간
+## 0.00ms~ 75.07msms
+
+completion = ["stanko", "ana", "mislav"]
+participant = ["mislav", "stanko", "mislav", "ana"]
+answer = ''
+
+completion.sort()
+participant.sort()
+
+for i in range(len(completion)):
+    if completion[i] != participant[i]:
+        print(participant[i])
+
+print(participant[len(participant)-1])

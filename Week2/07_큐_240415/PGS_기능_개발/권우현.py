4ef33ab842a4472f605896a23792a60df323b326
kwh <kwh871005@gmail.com>
2024-04-16 09:16:11 +0900

    [240415] 07장 큐 / 권우현

--- Week2/07_큐_240415/PGS_기능_개발/권우현.py
+++ Week2/07_큐_240415/PGS_기능_개발/권우현.py
@@ -1,11 +1,54 @@
 # 아이디어
-## 
+##남은날짜 계산후 speed로 나누고,
+##앞에서부터 확인, 바로 앞보다 짧게 걸리면 앞에 날로 변경해줌.
 
 # 예상 시간복잡도
-##
+## 10000번 연산
 
-# 풀이시간 
-## 분
+# 풀이시간
+## 15분
 
-# 실행시간 
-## ms~ ms
+# 실행시간
+## 0.01ms~ 0.05ms
+
+import time
+progresses = [1, 95, 95, 95]
+speeds = [99, 1, 1, 1]
+
+start = time.time()
+
+days = []
+
+for i in range(len(progresses)):
+    tmp = 100-progresses[i]
+    if tmp%speeds[i] !=0:
+        days.append(int(tmp//speeds[i])+1)
+    else :
+        days.append(tmp//speeds[i])
+
+prev = 0
+distribution = []
+answer = []
+
+for day in days:
+    if day > prev:
+        distribution.append(day)
+        prev = day
+    else :
+        distribution.append(prev)
+
+prev = distribution[0]
+tmp = 0
+for i in range(1,len(distribution)):
+    if(prev == distribution[i]):
+        tmp+=1
+    else :
+        prev = distribution[i]
+        answer.append(tmp+1)
+        tmp = 0
+
+answer.append(tmp+1)
+
+end = time.time()
+
+print(end - start)
--- Week2/07_큐_240415/PGS_카드_뭉치/권우현.py
+++ Week2/07_큐_240415/PGS_카드_뭉치/권우현.py
@@ -1,11 +1,38 @@
 # 아이디어
-## 
+## goal에 popleft로 빼고 card1, card2에 잇나 확인.
+
 
 # 예상 시간복잡도
 ##
 
-# 풀이시간 
-## 분
+# 풀이시간
+## 10분
 
-# 실행시간 
-## ms~ ms
+# 실행시간
+## 0.01ms~ 0.05ms
+
+from collections import deque
+
+cards1 = ["i", "water", "drink"]
+cards2 = ["want", "to"]
+goal = ["i", "want", "to", "drink", "water"]
+
+goal = deque(goal)
+flag = True
+
+while len(goal) > 0:
+    target = goal.popleft()
+    if cards1 and target == cards1[0]:
+        cards1.remove(target)
+    elif cards2 and target == cards2[0]:
+        cards2.remove(target)
+    else :
+        flag = False
+        break
+
+answer = ""
+if flag == True:
+    answer = "Yes"
+else : answer = "No"
+
+print(answer)

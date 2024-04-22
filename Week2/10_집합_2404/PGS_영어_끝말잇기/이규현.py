d30daf3daa89971efbaef646799b201fe0212284
kyu-hyun <102718303+lalabulla@users.noreply.github.com>
2024-04-22 14:44:40 +0900

    Fix : 코드 수정

--- Week2/10_집합_2404/PGS_영어_끝말잇기/이규현.py
+++ Week2/10_집합_2404/PGS_영어_끝말잇기/이규현.py
@@ -9,3 +9,18 @@
 
 # 실행시간 
 ## 0.01ms~ 0.03ms
+
+def solution(n, words):
+    answer = [0,0]
+    s = set()
+    last_char = words[0][0]
+    for idx, word in enumerate(words):
+
+        s.add(word)        
+        if len(s) != (idx+1) or last_char != word[0]:
+            answer[0] = (idx % n) + 1
+            answer[1] = (idx // n) + 1
+            break
+        last_char = word[-1]     
+        
+    return answer

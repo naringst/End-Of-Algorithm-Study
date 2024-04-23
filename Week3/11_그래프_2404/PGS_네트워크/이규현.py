19b31cd40a88db6fad5fcdb074c9d2ceaa9f7972
kyu-hyun <102718303+lalabulla@users.noreply.github.com>
2024-04-23 14:43:33 +0900

    [PGS] 네트워크 / lv3 / 35분 / 0.01ms / 이규현

--- Week3/11_그래프_2404/PGS_네트워크/이규현.py
+++ Week3/11_그래프_2404/PGS_네트워크/이규현.py
@@ -1,11 +1,40 @@
 # 아이디어
-## 
+## 연결된 노드 정보를 담은후 DFS를 해서 연결된 네트워크 카운트
 
 # 예상 시간복잡도
-##
+##O(n^2)
 
 # 풀이시간 
-## 분
+## 35분
 
 # 실행시간 
-## ms~ ms
+## 0.01ms~ 0.44ms
+
+def solution(n, computers):
+    answer = 0
+    visit = [0] * n
+    graph = [[] for _ in range(n)]
+
+    # 연결된 노드 추가 / 절반만 돌기 / 양방향
+    for i in range(n):
+        for j in range(i+1, n):
+            if computers[i][j] == 1:
+                graph[i].append(j)
+                graph[j].append(i)
+                
+    st = []
+    #DFS
+    for i in range(n):
+        if visit[i] == 0:
+            st.append(i)
+            visit[i] = 1
+            answer += 1
+            while st:
+                now = st.pop()
+                
+                for val in graph[now]:
+                    if visit[val] == 0:
+                        st.append(val)
+                        visit[val] = 1
+    
+    return answer

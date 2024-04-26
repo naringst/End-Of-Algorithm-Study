868600837cf894f2cba6ea029e3dbd7c57dec84f
dbswl701 <dbswl701@ajou.ac.kr>
2024-04-26 14:41:53 +0900

    [PGS] 정수 내림차순으로 배치하기 / lv1 / 2분 / 0.05ms / 강윤지

--- Week3/13_정렬_240426/PGS_정수_내림차순으로_배치하기/강윤지.py
+++ Week3/13_정렬_240426/PGS_정수_내림차순으로_배치하기/강윤지.py
@@ -1,11 +1,18 @@
 # 아이디어
-## 
+## 주석 참고
 
 # 예상 시간복잡도
-##
+## O(NlogN)
 
 # 풀이시간 
-## 분
+## 2분
 
 # 실행시간 
-## ms~ ms
+## 0.02ms~ 0.05ms
+
+def solution(n):
+    # 각 자릿수의 숫자를 따로따로 저장해서, 내림차순으로 정렬
+    temp = list(str(n))
+    temp2 = "".join(sorted(temp, reverse=True))
+
+    return int(temp2)

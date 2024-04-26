7956a5d5f7fdb92b980c54413fba0f02b803521b
kyu-hyun <102718303+lalabulla@users.noreply.github.com>
2024-04-26 14:37:14 +0900

    [PGS] K번째 수 / lv1 / 5분 / 0.00ms / 이규현

--- Week3/13_정렬_240426/PGS_K번째_수/이규현.py
+++ Week3/13_정렬_240426/PGS_K번째_수/이규현.py
@@ -1,11 +1,21 @@
 # 아이디어
-## 
+## 자르고 정렬
 
 # 예상 시간복잡도
-##
+## O(coomands 수)
 
 # 풀이시간 
-## 분
+## 5분
 
 # 실행시간 
-## ms~ ms
+## 0.00ms~ 0.01ms
+
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        s, e, idx = command
+        new_array = array[s-1:e]
+        new_array.sort()
+        answer.append(new_array[idx-1])
+    return answer

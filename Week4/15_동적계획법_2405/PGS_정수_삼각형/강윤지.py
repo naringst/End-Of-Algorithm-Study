513a5a6fcacc494cf6347c0b7d41817067cafd0d
dbswl701 <dbswl701@ajou.ac.kr>
2024-04-30 16:25:00 +0900

    [PGS] 정수 삼각형 / lv3 / 8분 / 50.73ms / 강윤지

--- Week4/15_동적계획법_2405/PGS_정수_삼각형/강윤지.py
+++ Week4/15_동적계획법_2405/PGS_정수_삼각형/강윤지.py
@@ -5,7 +5,18 @@
 ##
 
 # 풀이시간 
-## 분
+## 8분
 
 # 실행시간 
-## ms~ ms
+## 0.01ms~ 50.73ms
+
+def solution(triangle):
+    dp = triangle.copy()
+    depth = len(triangle)-1
+    # 아래에서부터 올라가면서 가장 큰 합으로 골라 올라가기
+    for i in range(1, len(triangle)):
+        # dp[x][y] 는 triangle[x][y] + max(dp[x+1][y], dp[x+1][y+1])
+        for j in range(len(triangle[depth-i])):
+            dp[depth-i][j] = triangle[depth-i][j] + max(dp[depth-i+1][j], dp[depth-i+1][j+1])
+    return dp[0][0]
+

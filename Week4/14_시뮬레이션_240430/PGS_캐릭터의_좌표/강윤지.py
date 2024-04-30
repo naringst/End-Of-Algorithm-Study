f6d1616c2807b32a300cd51b2e8c554ee1ab0a69
dbswl701 <dbswl701@ajou.ac.kr>
2024-04-30 15:54:30 +0900

    [PGS] 캐릭터의 좌표 / lv0 / 215분 / 0.02ms / 강윤지

--- Week4/14_시뮬레이션_240430/PGS_캐릭터의_좌표/강윤지.py
+++ Week4/14_시뮬레이션_240430/PGS_캐릭터의_좌표/강윤지.py
@@ -5,7 +5,22 @@
 ##
 
 # 풀이시간 
-## 분
+## 15분
 
 # 실행시간 
-## ms~ ms
+## 0.00ms~ 0.02ms
+
+def solution(keyinput, board):
+    width, height = 0, 0
+
+    for command in keyinput:
+        if command == 'left' and -(board[0]//2) < width:
+            width -= 1
+        elif command == 'right' and board[0]//2 > width:
+            width +=1
+        elif command == 'down' and -(board[1]//2) < height:
+            height -= 1
+        elif command == 'up' and board[1]//2 > height:
+            height +=1 
+            
+    return [width, height]

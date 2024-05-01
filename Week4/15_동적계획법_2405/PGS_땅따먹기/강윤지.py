e44f574f45839009db025063fad7872bf439f312
dbswl701 <dbswl701@ajou.ac.kr>
2024-05-01 14:42:36 +0900

    [PGS] 땅따먹기 / lv2 / 9분 / 209.26ms / 강윤지

--- Week4/15_동적계획법_2405/PGS_땅따먹기/강윤지.py
+++ Week4/15_동적계획법_2405/PGS_땅따먹기/강윤지.py
@@ -5,7 +5,18 @@
 ##
 
 # 풀이시간 
-## 분
+## 9분
 
 # 실행시간 
-## ms~ ms
+## 2.13ms~ 209.26ms
+
+def solution(land):
+    for i in range(1, len(land)):
+        # 직전 행 중 같은 열 제외한 것들 중 최댓값 찾기
+        for j in range(4):
+            temp = land[i-1][:j] + land[i-1][j+1:]
+            max_value = max(temp)
+            # 해당 칸 업데이트
+            land[i][j] += max_value
+
+    return max(land[-1])

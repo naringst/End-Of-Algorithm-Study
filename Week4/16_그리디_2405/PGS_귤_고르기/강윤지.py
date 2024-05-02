1d8bbafd8c18cf2115e9490e307d2d1217479b6f
dbswl701 <dbswl701@ajou.ac.kr>
2024-05-02 14:36:17 +0900

    [PGS] 귤 고르기 / lv2 / 15분 / 4425.58ms / 강윤지

--- Week4/16_그리디_2405/PGS_귤_고르기/강윤지.py
+++ Week4/16_그리디_2405/PGS_귤_고르기/강윤지.py
@@ -5,7 +5,29 @@
 ##
 
 # 풀이시간 
-## 분
+## 15분
 
 # 실행시간 
-## ms~ ms
+## 0.01ms~ 4425.58ms
+
+def solution(k, tangerine):
+    answer = 0
+    # 크기별로 dictionary를 이용하여 개수 카운팅
+    max_size = max(tangerine)
+    size_dict = dict(zip(range(1, max_size+1), [0]*max_size))
+    
+    # 카운팅
+    for size in tangerine:
+        size_dict[size] += 1
+    
+    # value 내림차순으로 정렬
+    size_dict = dict(sorted(size_dict.items(), key=lambda x: x[1], reverse=True))
+    
+    # 앞에서부터 순서대로 사용
+    for value in size_dict.values():
+        k -= value
+        answer += 1
+        if k <= 0:
+            return answer
+
+    return answer
